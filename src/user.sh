8946a49dddc59d722a97049658599df2da10c1b0
Cheul Young Park <cheulyop@gmail.com>
2022-03-07 23:41:54 +0000

    feat: bump base image version

--- src/user.sh
+++ src/user.sh
@@ -13,7 +13,11 @@ echo "${SSH_MASTER_USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/nopasswd
 # echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/useradd" >> /etc/sudoers
 # echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/deluser" >> /etc/sudoers
 # echo "${SSH_MASTER_USER} ALL=NOPASSWD:/usr/sbin/chpasswd" >> /etc/sudoers
- 
+
+printf "\n\033[0;44m---> Creating groups \"sftp\" and \"dataman\".\033[0m\n" 
 addgroup sftp
- 
+addgroup dataman --gid 1005
+usermod -aG dataman ${SSH_MASTER_USER}
+usermod -g dataman ${SSH_MASTER_USER}
+
 exec "$@"
